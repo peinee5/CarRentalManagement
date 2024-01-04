73ee9b21edd3fb7af5935a7062d941df0661b132
CHEN PEI NEE 2203596H <2203596H@student.tp.edu.sg>
2024-01-04 22:14:20 +0800

    Added Blazor List Pages with Delete function.

--- CarRentalManagement/Server/Controllers/BookingsController.cs
+++ CarRentalManagement/Server/Controllers/BookingsController.cs
@@ -41,7 +41,7 @@ namespace CarRentalManagement.Server.Controllers
           //}
             //Refactored
             //return await _context.Bookings.ToListAsync();
-            var Bookings = await _unitOfWork.Bookings.GetAll();
+            var Bookings = await _unitOfWork.Bookings.GetAll(includes: q => q.Include(x => x.Vehicle).Include(x => x.Customer));
             return Ok(Bookings);
         }
 
